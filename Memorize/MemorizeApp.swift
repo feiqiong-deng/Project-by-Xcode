48d554394b8861fff1e31f2f5d5349e29174e6b3
FeiqiongDeng <feiqiongd@kingsbridgebcp.com>
2021-04-30 22:28:07 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 邓飞琼 on 2021-04-30.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
